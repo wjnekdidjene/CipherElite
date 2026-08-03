40605a612583a85bbf8e96a4abd1d9fe99736870
wjnekdidjene <terazm2007@gmail.com>
2026-08-04 02:00:05 +0300

    Add files via upload

---
+++ config/config.py
@@ -0,0 +1,19 @@
+from vars import config
+
+
+class Config:
+    API_ID = config.API_ID
+    API_HASH = config.API_HASH
+    ELITE_SESSION = config.ELITE_SESSION
+    BOT_TOKEN = config.BOT_TOKEN
+    TG_BOT_USERNAME = config.ELITE_BOT_USERNAME
+    BOT_PREFIX = config.BOT_PREFIX
+    SUDO_USERS = config.SUDO_USERS
+    LOG_CHAT_ID = config.LOG_CHAT_ID
+    ALIVE_NAME = config.ALIVE_NAME
+    LANGUAGE = config.LANGUAGE
+    BRANCH = config.BRANCH
+    VERSION = "2.0.0"
+    MONGO_URI = config.MONGO_URI
+    UPSTREAM_REPO = config.UPSTREAM_REPO
+    OWNER_ID = None
