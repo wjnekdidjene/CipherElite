8eb97c3da538b4c6337f077745c810ca84004466
wjnekdidjene <terazm2007@gmail.com>
2026-08-04 03:12:16 +0300

    Add files via upload

---
+++ utils/utils.py
@@ -0,0 +1,9 @@
+CipherElite = None
+
+def init_client(client_instance):
+    global CipherElite
+    CipherElite = client_instance
+
+def get_client():
+    global CipherElite
+    return CipherElite
